f57fc16c824e29de59bec05aa2955dd3cc979257
Deepraj <deepraajdas@gmail.com>
2024-04-06 18:42:43 +0530

    feat - router setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,21 @@
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./features/authentication/screens/Login";
+import Signup from "./features/authentication/screens/Signup";
+import Navbar from "./core/components/Navbar";
+import Home from "./features/home/screens/Home";
 
 function App() {
   return (
-    <div className="App text-3xl font-bold h-screen w-screen flex justify-center items-center">
-       Ravi Daa ROBI ROBI
+    <div className="App h-screen w-screen">
+      <Navbar />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/signup" element={<Signup />}></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
