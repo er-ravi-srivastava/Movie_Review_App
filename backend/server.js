d127749d78aafd6406b49cc8c5bf4afc4daa3e46
er-ravi-srivastava <raviprakashshrivastav7@gmail.com>
2024-04-07 11:50:16 +0530

    Updated backend (mongodb)

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,16 @@
-console.log("Ravi c");
+const mongoose = require("mongoose");
+
+const url = "mongodb+srv://raviprakashshrivastav7:Ravi%402000@cluster0.b8god0x.mongodb.net/";
+
+const connectDB = () => {
+    console.log("connect ho raha");
+    return mongoose.connect(url).then(() => {
+        console.log("ravi da!");
+    })
+    .catch((err) => {
+        console.error("deepraj:", err);
+    });
+
+};
+connectDB();
+module.exports = connectDB;
