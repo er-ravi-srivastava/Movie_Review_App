58fe7b36718b92885ea2a597b081619d17054207
er-ravi-srivastava <raviprakashshrivastav7@gmail.com>
2024-04-08 08:22:33 +0530

    Updates

--- frontend/src/features/home/screens/Home.js
+++ frontend/src/features/home/screens/Home.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Home = () => {
   return (
-<h1 className="mt-40" style={{textAlign: "center", fontWeight: "bold"}}>Welcome to MovieMania - Your Ultimate Destination for Film Reviews!</h1>
+<h3 className="mt-40" style={{textAlign: "center", fontWeight: "bold"}}>Welcome to MovieMania - Your Ultimate Destination for Film Reviews!</h3>
 
   )
 }
