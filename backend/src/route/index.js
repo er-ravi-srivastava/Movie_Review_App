24dcd4c3b6705913d2d5b4cf4df425e099839a24
Deepraj <deepraajdas@gmail.com>
2024-04-08 22:31:02 +0530

    optionals

--- backend/src/route/index.js
+++ backend/src/route/index.js
@@ -4,9 +4,19 @@ import db from "../db/Schema.js";
 const router = express.Router();
 
 router.get("/movies", async (req, res) => {
-    const movies = db.Movie.find({});
-    console.log(movies)
-    res.json({ data: { movie: [] } });
+  const movies = await db.Movie.find({});
+  console.log(movies)
+  const movieMapp = movies.map((movie) => {
+    console.log("first");
+    return {
+      _id: movie._id.toString(),
+      title: movie.title,
+      poster: movie.poster,
+      summary: movie.summary,
+    };
+  });
+  console.log(movieMapp);
+  res.json({ data: { movie: [] } });
 });
 
 export default router;
