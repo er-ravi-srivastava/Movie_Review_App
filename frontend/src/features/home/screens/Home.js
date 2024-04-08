d599fe6f5df04cf4b2c0afc86de64155f04c627a
er-ravi-srivastava <raviprakashshrivastav7@gmail.com>
2024-04-08 10:49:10 +0530

    updates

--- frontend/src/features/home/screens/Home.js
+++ frontend/src/features/home/screens/Home.js
@@ -8,13 +8,13 @@ const Home = () => {
         <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
         Welcome to MovieMania - Your Ultimate Destination for Film Reviews!
         </h1>
-        <p className="mt-3 text-lg leading-8 text-black-600">
+        <p className="mt-0 text-lg leading-8 text-black-600 px-10">
 
 "Welcome to MovieMania" - Your Ultimate Destination for Film Reviews! Immerse yourself in the mesmerizing world of cinema, where every frame tells a story and every performance captivates the soul. At MovieMania, we're passionate about bringing you the latest insights, reviews, and analyses of the silver screen's finest offerings.
 
 Embark on a cinematic adventure like never before as you explore a vast array of genres, from pulse-pounding action thrillers to heartwarming romantic comedies and thought-provoking dramas. Discover hidden cinematic gems waiting to be unearthed and revisit timeless classics that have shaped the landscape of modern filmmaking.
         </p>
-        <div className="mt-10 flex items-center justify-center gap-x-6">
+        <div className="mt-2 flex items-center justify-center gap-x-6">
           <a
             href="#"
             className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
