9c57e3db446bc5cd288214aa724c3d79fb9551e6
er-ravi-srivastava <raviprakashshrivastav7@gmail.com>
2024-04-09 08:14:31 +0530

    updates

--- frontend/src/features/movies/screens/Movies.js
+++ frontend/src/features/movies/screens/Movies.js
@@ -9,7 +9,7 @@ const Movie = () => {
     const fetchMovies = async () => {
       try {
         const response = await axios.get("http://localhost:3008/movies");
-        console.log(response.data)
+        // console.log(response.data)
         setMovies(response.data.data.movie)
       } catch (error) {
         console.error("Error fetching movies:", error);
