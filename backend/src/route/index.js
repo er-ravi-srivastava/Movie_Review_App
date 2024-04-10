8a30c532a74d9782f654f344bd6025105f7961e7
er-ravi-srivastava <raviprakashshrivastav7@gmail.com>
2024-04-10 19:22:31 +0530

    update - login , register API implementation

--- backend/src/route/index.js
+++ backend/src/route/index.js
@@ -3,6 +3,46 @@ import db from "../db/Schema.js";
 
 const router = express.Router();
 
+
+router.post("/login", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const user = await db.Users.findOne({username,password});
+    if(!user){
+      throw new Error("Invalid credentials")
+      }
+    else{
+      res.status(200).json({message:"login successfull"})
+    }
+  } catch (error) {
+    res.status(403).json({ message: error.message });
+
+  }
+});
+
+router.post("/register", async (req, res) => {
+  const { username, password } = req.body;
+  
+  try {
+    const user = await db.Users.findOne({ username});
+    
+    if (user) {
+      return res.status(400).json({ message: 'User already exists' });
+    }
+    
+    
+    const newUser = new db.Users({ username, password });
+    await newUser.save();
+    
+    res.status(201).json({ message: 'User created successfully' });
+  } catch (error) {
+    console.error('Error registering user:', error);
+    res.status(500).json({ message: 'Server Error' });
+  }
+});
+
+    
+
 router.get("/movies", async (req, res) => {
   const movies = await db.Movie.find({});
   const movieMapp = movies.map((movie) => {
