2c1be4281df9c76361ae6e20460cda169e46b023
er-ravi-srivastava <raviprakashshrivastav7@gmail.com>
2025-03-08 11:04:48 +0530

    Updates

--- backend/src/db/index.js
+++ backend/src/db/index.js
@@ -3,16 +3,12 @@ import mongoose from "mongoose";
 const connectDB = async () => {
   try {
     await mongoose.connect(
-      "mongodb+srv://raviprakashshrivastav7:Ravi%402000@cluster0.b8god0x.mongodb.net/movie_user_login_data",
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      }
+      "mongodb+srv://raviprakashshrivastav7:Ravi%402000@cluster0.b8god0x.mongodb.net/movie_user_login_data"
     );
-    console.log("Connected to the database");
+    console.log("✅ Connected to MongoDB");
   } catch (error) {
-    console.error("Unable to connect to the database:", error);
-    throw new Error("Could not connect to MongoDB");
+    console.error("❌ Unable to connect to MongoDB:", error.message);
+    process.exit(1); // Exit the process if the connection fails
   }
 };
 
--- backend/src/server.js
+++ backend/src/server.js
@@ -4,26 +4,27 @@ import cors from "cors";
 import router from "./route/index.js";
 
 const app = express();
-const PORT =  3008;
+const PORT = 3008;
 
 connectDB();
 
-app.use(cors(
-{
-  origin: ["https://movie-review-api.vercel.app/"]
-  method: ["POST","GET","PUT","PATCH"],
-  credentials: true
-}
-));
+
+app.use(cors())
+
+// app.use(cors({
+//   origin: "https://movie-review-api.vercel.app/",
+//   methods: ["POST", "GET", "PUT", "PATCH"],
+//   credentials: true
+// }));
+
 app.use(express.json());
-app.use(router);
 
-
-app.listen(PORT, () => {
-  console.log(PORT);
-  console.log("It is been connected now ");
-});
 app.get("/", (req, res) => {
   res.json({ message: "Hello from Vercel!" });
 });
 
+app.use(router);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
